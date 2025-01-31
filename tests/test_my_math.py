c403d695660ea0f8555dd39a051b8c218539e20e
Brendon <brendonc@andrews.edu>
2025-01-31 11:19:37 -0500

    End Class Fri Jan 31

--- tests/test_my_math.py
+++ tests/test_my_math.py
@@ -9,9 +9,17 @@ def test_sum():
 # Work together
 ## Test multiply
 
+def test_mult():
+    res = multiply(2,2)
+    assert res == 4
+
 
 # Check for understanding
 ## Test difference
+
+def test_diff():
+    res = difference(2,1)
+    assert res == 1
 
 
 # Work together
